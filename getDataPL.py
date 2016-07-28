8a21d9fdda7599140b670249900d83b5ba8002b0
hgmatt <matthew.madden@gmail.com>
2016-07-27 21:41:51 -0400

    First Committ

---
+++ getDataPL.py
@@ -0,0 +1,24 @@
+import requests, cPickle, shutil, time
+
+all = {}
+errorout = open('errors.log', 'w')
+
+date_text = time.strftime("%Y_%m_%d")
+
+all_data_url = 'https://fantasy.premierleague.com/drf/bootstrap-static'
+r_all = requests.get(all_data_url)
+all_data = r_all.json()
+cPickle.dump(all_data, open(date_text + '_all_data.p','wb'))
+
+for i in range(600):
+    playerurl = 'http://fantasy.premierleague.com/web/api/elements/%s/'
+    r = requests.get(playerurl % i)
+
+    # skip non-existent players
+    if r.status_code != 200: continue
+
+    all[i] = r.json()
+    print i
+print date_text
+
+cPickle.dump(all, open(date_text + '_player_data.p', 'wb'))
